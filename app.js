e277b3c991792366b726fbda55754f09745f1e65
desmu <90912716+desmu@users.noreply.github.com>
2021-12-08 17:19:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const geoCode = require("./geoCode");
 const forecast = require("./forecast");
 
-console.log("Enter : 4 args :city,(coordonnées ou ville),latitude,longitude([void] si pas d'argument \nSi 'ville' ajouter full pour toutes les infos")
+console.log("Entrer : 4 args :city,(coordonnées ou ville),latitude,longitude([void] si pas d'argument) \nSi 'ville' ajouter full pour toutes les infos")
 
 const stdin = process.openStdin();
 
@@ -18,7 +18,7 @@ const tenki = async (city,type,lat,lon) =>{
 }
 stdin.addListener("data", function (st) {
     const city = (st.toString().split(",",4))[0];
-    const type = (st.toString().split(",",4))[1];;
+    const type = (st.toString().split(",",4))[1];
     const lat = (st.toString().split(",",4))[2];
     const lon = (st.toString().split(",",4))[3];
     tenki(city,type,lat,lon);
