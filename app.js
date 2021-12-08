0e12707ee3e5fff33fa93707be0f4ff72f16b4aa
desmu <90912716+desmu@users.noreply.github.com>
2021-12-08 18:03:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const forecast = require("./forecast");
 
 const stdin = process.openStdin();
 
-console.log("SVP Entez soit : {nom de la vile} + full(pour plus d'infos),ville \nsoit : latitude,longitude")
+console.log("SVP Entez soit : {nom de la vile} + full(pour plus d'infos),ville \nsoit : latitude,longitude\nTo exit type 'exit'\n\n")
 
 const tenki = async (city,lat,lon,len) =>{
     if(len === 1){
@@ -19,15 +19,17 @@ const tenki = async (city,lat,lon,len) =>{
 }
 stdin.addListener("data", function (st) {
     const argLen = (st.toString().split(",",4)).length
-    if(argLen === 1){
+    if(st.toString().slice(0, st.length - 2) === 'exit'){
+        process.exit();
+    }else if(argLen === 1){
         const city = (st.toString().split(",",4))[0];
         tenki(city,"void","void",argLen);
     }else if(argLen === 2){
-        const lat = (st.toString().split(",",4))[1];
-        const lon = (st.toString().split(",",4))[2];
+        const lat = (st.toString().split(",",4))[0];
+        const lon = (st.toString().split(",",4))[1];
         tenki("void",lat,lon,argLen);
     }else{
-        console.log("SVP Entez soit : {nom de la vile} + full(pour plus d'infos),ville \nsoit : coordonnées,latitude,longitude")
+        console.log("SVP Entez soit : {nom de la vile} + full(pour plus d'infos),ville \nsoit : coordonnées,latitude,longitude\nTo exit type 'exit'\n\n")
     }
     
 })
