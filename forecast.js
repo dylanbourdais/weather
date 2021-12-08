0dff3bc02bb7c5243eda1539661c6c1397e07276
desmu <90912716+desmu@users.noreply.github.com>
2021-12-08 18:27:18 +0100

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -6,7 +6,7 @@ module.exports =  async(lat,lon) => {
     require('dotenv').config();
     const axios = require("axios");
     const {data : weather} = await axios(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.CLIENT_TOKEN}`);
-    console.log(`Il fait ${(weather.current.temp - 273.15).toFixed(2)} degrés celcius à ${(weather.timezone).split("/",2)[1]}`)
+    console.log(`Il fait ${(weather.current.temp - 273.15).toFixed(2)} degrés celcius à ces coordonnées\nTimezone de : ${(weather.timezone).split("/",2)[1]}`);
     console.log("\nSVP Entez soit : {nom de la vile} + full(pour plus d'infos),ville \nsoit : latitude,longitude\nTo exit type 'exit'\n\n")
   }catch(err){
     console.log("\n"+err.message + "\nSVP Entez soit : {nom de la vile} + full(pour plus d'infos),ville \nsoit : latitude,longitude\nTo exit type 'exit'\n\n")
