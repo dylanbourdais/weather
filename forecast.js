fb5a0273b66412b922a727699af0e4134d331cdd
desmu <90912716+desmu@users.noreply.github.com>
2021-12-08 17:44:09 +0100

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -8,7 +8,7 @@ module.exports =  async(lat,lon) => {
     const {data : weather} = await axios(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.CLIENT_TOKEN}`);
     console.log(`Il fait ${(weather.current.temp - 273.15).toFixed(2)}`)
   }catch(err){
-    console.log("\n"+err.message + "Enter : 4 args :city,(coordonnées ou ville),latitude,longitude([void] si pas d'argument \nSi 'ville' ajouter full pour toutes les infos")
+    console.log("\n"+err.message + "\nSVP Entez soit : {nom de la vile} + full(pour plus d'infos),ville \nsoit : latitude,longitude")
   }
   
 }
