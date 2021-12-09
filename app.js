3c1675c73403bdfa809f02e98e210abe7d9c8724
desmu <90912716+desmu@users.noreply.github.com>
2021-12-09 09:16:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,18 +18,18 @@ const tenki = async (city,lat,lon,len) =>{
 
 }
 stdin.addListener("data", function (st) {
-    const argLen = (st.toString().split(",",4)).length
+    const argLen = (st.toString().split(",",3)).length
     if(st.toString().slice(0, st.length - 2) === 'exit'){
         process.exit();
     }else if(argLen === 1){
-        const city = (st.toString().split(",",4))[0];
+        const city = st.toString();
         tenki(city,"void","void",argLen);
     }else if(argLen === 2){
-        const lat = (st.toString().split(",",4))[0];
-        const lon = (st.toString().split(",",4))[1];
+        const lat = (st.toString().split(",",2))[0];
+        const lon = (st.toString().split(",",2))[1];
         tenki("void",lat,lon,argLen);
     }else{
-        console.log("SVP Entez soit : {nom de la vile} + full(pour plus d'infos)\nsoit : coordonnées,latitude,longitude\nTo exit type 'exit'\n\n")
+        console.log("SVP Entez soit : {nom de la vile} + full(pour plus d'infos) \nsoit : coordonnées,latitude,longitude\nTo exit type 'exit'\n\n")
     }
     
 })
